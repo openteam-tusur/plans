25d98a5e72709194f24ce70352fa1059eb60cea5
Evgeny Lapin <lev@openteam.ru>
2012-07-25 16:58:55 +0700

    поправлены виды управления ГОСами и исправлен баг в сидах

--- db/seeds.rb
+++ db/seeds.rb
@@ -128,258 +128,258 @@ end
 
 Gos.find_or_initialize_by_speciality_code('090105.65') do |gos|
   gos.update_attributes :code => '075500',
-                            :ititle => 'Комплексное обеспечение информационной безопасности автоматизированных систем',
+                            :title => 'Комплексное обеспечение информационной безопасности автоматизированных систем',
                             :approved_on => '2000-04-05'
 end
 
 Gos.find_or_initialize_by_speciality_code('090106.65') do |gos|
   gos.update_attributes :code => '075600',
-                            :ititle => 'Информационная безопасность телекоммуникационных систем',
+                            :title => 'Информационная безопасность телекоммуникационных систем',
                             :approved_on => '2000-04-05'
 end
 
 Gos.find_or_initialize_by_speciality_code('100101.65') do |gos|
   gos.update_attributes :code => '230700',
-                            :ititle => 'Сервис',
+                            :title => 'Сервис',
                             :approved_on => '2000-03-27'
 end
 
 Gos.find_or_initialize_by_speciality_code('100101.65') do |gos|
   gos.update_attributes :code => '230700',
-                            :ititle => 'Сервис',
+                            :title => 'Сервис',
                             :approved_on => '2000-03-27'
 end
 
 Gos.find_or_initialize_by_speciality_code('160905.65') do |gos|
   gos.update_attributes :code => '653300',
-                            :ititle => 'Эксплуатация транспорта и транспортного оборудования',
+                            :title => 'Эксплуатация транспорта и транспортного оборудования',
                             :approved_on => '2000-04-05'
 end
 
 Gos.find_or_initialize_by_speciality_code('200600.62') do |gos|
   gos.update_attributes :code => '200600',
-                            :ititle => 'Фотоника и оптоинформатика',
+                            :title => 'Фотоника и оптоинформатика',
                             :approved_on => '2005-12-05'
 end
 
 Gos.find_or_initialize_by_speciality_code('200600.68') do |gos|
   gos.update_attributes :code => '200600',
-                            :ititle => 'Фотоника и оптоинформатика',
+                            :title => 'Фотоника и оптоинформатика',
                             :approved_on => '2005-12-05'
 end
 
 Gos.find_or_initialize_by_speciality_code('210100.62') do |gos|
   gos.update_attributes :code => '550700',
-                            :ititle => 'Электроника и микроэлектроника',
+                            :title => 'Электроника и микроэлектроника',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('210100.68') do |gos|
   gos.update_attributes :code => '550700',
-                            :ititle => 'Электроника и микроэлектроника',
+                            :title => 'Электроника и микроэлектроника',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('210104.65') do |gos|
   gos.update_attributes :code => '210100',
-                            :ititle => 'Электроника и микроэлектроника',
+                            :title => 'Электроника и микроэлектроника',
                             :approved_on => '2006-01-18'
 end
 
 Gos.find_or_initialize_by_speciality_code('210105.65') do |gos|
   gos.update_attributes :code => '210100',
-                            :ititle => 'Электроника и микроэлектроника',
+                            :title => 'Электроника и микроэлектроника',
                             :approved_on => '2006-01-18'
 end
 
 Gos.find_or_initialize_by_speciality_code('210106.65') do |gos|
   gos.update_attributes :code => '210100',
-                            :ititle => 'Электроника и микроэлектроника',
+                            :title => 'Электроника и микроэлектроника',
                             :approved_on => '2006-01-18'
 end
 
 Gos.find_or_initialize_by_speciality_code('210201.65') do |gos|
   gos.update_attributes :code => '654300',
-                            :ititle => 'Проектирование и технология радиоэлектронных средств',
+                            :title => 'Проектирование и технология радиоэлектронных средств',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('210202.65') do |gos|
   gos.update_attributes :code => '654300',
-                            :ititle => 'Проектирование и технология радиоэлектронных средств',
+                            :title => 'Проектирование и технология радиоэлектронных средств',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('210300.62') do |gos|
   gos.update_attributes :code => '552500',
-                            :ititle => 'Радиотехника',
+                            :title => 'Радиотехника',
                             :approved_on => '2000-03-17'
 end
 
 Gos.find_or_initialize_by_speciality_code('210300.68') do |gos|
   gos.update_attributes :code => '552500',
-                            :ititle => 'Радиотехника',
+                            :title => 'Радиотехника',
                             :approved_on => '2000-03-17'
 end
 
 Gos.find_or_initialize_by_speciality_code('210302.65') do |gos|
   gos.update_attributes :code => '654200',
-                            :ititle => 'Радиотехника',
+                            :title => 'Радиотехника',
                             :approved_on => '2000-03-17'
 end
 
 Gos.find_or_initialize_by_speciality_code('210303.65') do |gos|
   gos.update_attributes :code => '654200',
-                            :ititle => 'Радиотехника',
+                            :title => 'Радиотехника',
                             :approved_on => '2000-03-17'
 end
 
 Gos.find_or_initialize_by_speciality_code('210304.65') do |gos|
   gos.update_attributes :code => '654200',
-                            :ititle => 'Радиотехника',
+                            :title => 'Радиотехника',
                             :approved_on => '2000-03-17'
 end
 
 Gos.find_or_initialize_by_speciality_code('210312.65') do |gos|
   gos.update_attributes :code => '654200',
-                            :ititle => 'Радиотехника',
+                            :title => 'Радиотехника',
                             :approved_on => '2000-03-17'
 end
 
 Gos.find_or_initialize_by_speciality_code('210400.62') do |gos|
   gos.update_attributes :code => '550400',
-                            :ititle => 'Телекоммуникации',
+                            :title => 'Телекоммуникации',
                             :approved_on => '2000-02-23'
 end
 
 Gos.find_or_initialize_by_speciality_code('210400.68') do |gos|
   gos.update_attributes :code => '550400',
-                            :ititle => 'Телекоммуникации',
+                            :title => 'Телекоммуникации',
                             :approved_on => '2000-02-23'
 end
 
 Gos.find_or_initialize_by_speciality_code('210401.65') do |gos|
   gos.update_attributes :code => '654400',
-                            :ititle => 'Телекоммуникации',
+                            :title => 'Телекоммуникации',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('210402.65') do |gos|
   gos.update_attributes :code => '654400',
-                            :ititle => 'Телекоммуникации',
+                            :title => 'Телекоммуникации',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('210403.65') do |gos|
   gos.update_attributes :code => '654400',
-                            :ititle => 'Телекоммуникации',
+                            :title => 'Телекоммуникации',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('210405.65') do |gos|
   gos.update_attributes :code => '654400',
-                            :ititle => 'Телекоммуникации',
+                            :title => 'Телекоммуникации',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('210600.62') do |gos|
   gos.update_attributes :code => '210600',
-                            :ititle => 'Нанотехнология',
+                            :title => 'Нанотехнология',
                             :approved_on => '2006-01-18'
 end
 
 Gos.find_or_initialize_by_speciality_code('220100.62') do |gos|
   gos.update_attributes :code => '553000',
-                            :ititle => 'Системный анализ и управление',
+                            :title => 'Системный анализ и управление',
                             :approved_on => '2000-04-14'
 end
 
 Gos.find_or_initialize_by_speciality_code('220200.62') do |gos|
   gos.update_attributes :code => '550200',
-                            :ititle => 'Автоматизация и управление',
+                            :title => 'Автоматизация и управление',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('220200.68') do |gos|
   gos.update_attributes :code => '550200',
-                            :ititle => 'Автоматизация и управление',
+                            :title => 'Автоматизация и управление',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('220201.65') do |gos|
   gos.update_attributes :code => '651900',
-                            :ititle => 'Автоматизация и управление',
+                            :title => 'Автоматизация и управление',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('220301.65') do |gos|
   gos.update_attributes :code => '651900',
-                            :ititle => 'Автоматизация и управление',
+                            :title => 'Автоматизация и управление',
                             :approved_on => '2000-03-10'
 end
 
 Gos.find_or_initialize_by_speciality_code('220501.65') do |gos|
   gos.update_attributes :code => '657000',
-                            :ititle => 'Управление качеством',
+                            :title => 'Управление качеством',
                             :approved_on => '2000-03-27'
 end
 
 Gos.find_or_initialize_by_speciality_code('220000.62') do |gos|
   gos.update_attributes :code => '220600',
-                            :ititle => 'Инноватика',
+                            :title => 'Инноватика',
                             :approved_on => '2005-12-20'
 end
 
 Gos.find_or_initialize_by_speciality_code('220600.68') do |gos|
   gos.update_attributes :code => '220600',
-                            :ititle => 'Инноватика',
+                            :title => 'Инноватика',
                             :approved_on => '2005-12-20'
 end
 
 Gos.find_or_initialize_by_speciality_code('220601.65') do |gos|
   gos.update_attributes :code => '220601',
-                            :ititle => 'Управление инновациями',
+                            :title => 'Управление инновациями',
                             :approved_on => '2005-12-20'
 end
 
 Gos.find_or_initialize_by_speciality_code('230100.62') do |gos|
   gos.update_attributes :code => '552800',
-                            :ititle => 'Информатика и вычислительная техника',
+                            :title => 'Информатика и вычислительная техника',
                             :approved_on => '2000-03-13'
 end
 
 Gos.find_or_initialize_by_speciality_code('230100.68') do |gos|
   gos.update_attributes :code => '552800',
-                            :ititle => 'Информатика и вычислительная техника',
+                            :title => 'Информатика и вычислительная техника',
                             :approved_on => '2000-03-13'
 end
 
 Gos.find_or_initialize_by_speciality_code('230102.65') do |gos|
   gos.update_attributes :code => '654600',
-                            :ititle => 'Информатика и вычислительная техника',
+                            :title => 'Информатика и вычислительная техника',
                             :approved_on => '2000-03-27'
 end
 
 Gos.find_or_initialize_by_speciality_code('230104.65') do |gos|
   gos.update_attributes :code => '654600',
-                            :ititle => 'Информатика и вычислительная техника',
+                            :title => 'Информатика и вычислительная техника',
                             :approved_on => '2000-03-27'
 end
 
 Gos.find_or_initialize_by_speciality_code('230105.65') do |gos|
   gos.update_attributes :code => '654600',
-                            :ititle => 'Информатика и вычислительная техника',
+                            :title => 'Информатика и вычислительная техника',
                             :approved_on => '2000-03-27'
 end
 
 Gos.find_or_initialize_by_speciality_code('230200.62') do |gos|
   gos.update_attributes :code => '230200',
-                            :ititle => 'Информационные системы',
+                            :title => 'Информационные системы',
                             :approved_on => '2005-12-23'
 end
 
 Gos.find_or_initialize_by_speciality_code('280101.65') do |gos|
   gos.update_attributes :code => '656500',
-                            :ititle => 'Безопасность жизнедеятельности',
+                            :title => 'Безопасность жизнедеятельности',
                             :approved_on => '2000-04-05'
 end
