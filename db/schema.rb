d422fa04cae0b2bf8c52e7372672411f34ebca2b
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-07-30 15:36:01 +0700

    merge db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730050517) do
+ActiveRecord::Schema.define(:version => 20120730055030) do
 
   create_table "checks", :force => true do |t|
     t.integer  "semester_id"
