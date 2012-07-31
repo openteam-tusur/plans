0c01d426233b52d42219762cae1b3a3c30bd7c7b
Dmitry Lihachev <lda@openteam.ru>
2012-07-31 09:37:48 +0700

    Самостоятельная работа. лабораторные работы

--- app/models/self_education.rb
+++ app/models/self_education.rb
@@ -1,12 +1,17 @@
 class SelfEducation < ActiveRecord::Base
   belongs_to :work_programm
-  attr_accessible :lecture_control, :lecture_hours
+  attr_accessible :lecture_control, :lecture_hours, :lab_control, :lab_hours
 
   validates :lecture_hours, :presence => true, :numericality => { :only_integer => true, :greater_than => 0 }, :if => :need_lecture?
+  validates :lecture_control, :presence => true, :if => :need_lecture?
+  validates :lab_hours, :presence => true, :numericality => { :only_integer => true, :greater_than => 0 }, :if => :need_lab?
+  validates :lab_control, :presence => true, :if => :need_lab?
 
-  has_enum :lecture_control, :multiple => true
+  has_enums
 
-  def need_lecture?
-    work_programm.has_loadings_for? :lecture
+  Exercise.enum_values(:kind).each do |value|
+    define_method "need_#{value}?" do
+      work_programm.has_loadings_for? value
+    end
   end
 end
