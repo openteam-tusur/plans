52395b90b7e48cab3a12a434cf2c2a2588f4a0c5
Dmitry Lihachev <lda@openteam.ru>
2012-08-01 12:58:57 +0700

    Улучшена генерация PDF

--- app/models/appendix.rb
+++ app/models/appendix.rb
@@ -2,4 +2,6 @@ class Appendix < ActiveRecord::Base
   belongs_to :work_programm
   attr_accessible :title, :kind
   scope :kind, ->(kind) { where(:kind => kind) }
+
+  ALL_KINDS   = %w[lab practice csr home_work referat test colloquium calculation srs exam]
 end
