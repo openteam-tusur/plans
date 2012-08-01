e5baedbec2a901c95ad20bc2153fdc6fcf74c848
Dmitry Lihachev <lda@openteam.ru>
2012-08-01 11:00:01 +0700

    SelfEducation д.б. указаны часы если указана форма контроля

--- app/models/self_education.rb
+++ app/models/self_education.rb
@@ -6,30 +6,24 @@ class SelfEducation < ActiveRecord::Base
   FIFTH_ITEM_KINDS = %w[home_work referat test colloquium calculation]
   ALL_KINDS   = %w[lecture lab practice csr home_work referat test colloquium calculation srs exam]
 
-  FIFTH_ITEM_KINDS.each do |kind|
+  ALL_KINDS.each do |kind|
     attr_accessible "#{kind}_hours", "#{kind}_control"
-
     validates "#{kind}_control", :presence => true, :if => "#{kind}_hours?"
+    default_value_for "#{kind}_hours", 0
+    validates "#{kind}_hours", :numericality => { :only_integer => true, :greater_than  => 0 }, :if => "#{kind}_control?"
+  end
 
+  FIFTH_ITEM_KINDS.each do |kind|
     validates "#{kind}_hours",
               :presence => true,
               :numericality => { :only_integer => true, :greater_than_or_equal_to  => 0 }
-
-    default_value_for "#{kind}_hours", 0
   end
 
   Loading.enum_values(:loading_kind).each do |kind|
-    attr_accessible "#{kind}_hours", "#{kind}_control"
-
-    validates "#{kind}_control", :presence => true, :if => "#{kind}_hours?"
-
     validates "#{kind}_hours",
               :presence => true,
               :numericality => { :only_integer => true, :greater_than_or_equal_to  => 0 },
               :if => :"need_#{kind}?"
-
-    default_value_for "#{kind}_hours", 0
-
     define_method "need_#{kind}?" do
       work_programm.has_loadings_for? kind
     end
