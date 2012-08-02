717e3f4479394564977645dd9f456c070cbcfeed
Dmitry Lihachev <lda@openteam.ru>
2012-08-02 11:44:27 +0700

    формы контроля для СРС

--- app/models/self_education_item.rb
+++ app/models/self_education_item.rb
@@ -5,4 +5,24 @@ class SelfEducationItem < ActiveRecord::Base
   attr_accessible :control, :hours, :kind
 
   scope :kind, ->(kind) { where :kind => kind }
+
+  has_enum :control, :multiple => true
+
+  AVAILABLE_CONTROLS = {
+        lecture:      [:quiz, :test, :home_work],
+        lab:          [:access, :defence],
+        practice:     [:quiz, :test, :check],
+        csr:          [:report],
+        home_work:    [:defence, :mark],
+        referat:      [:defence, :mark],
+        test:         [:check, :mark],
+        colloquium:   [:defence, :mark],
+        calculation:  [:defence, :mark],
+        srs:          [:check_summary, :quiz, :test, :home_work],
+        exam:         [:pass]
+  }
+
+  def self.available_human_control_values(kind)
+    human_enum_values(:control).keep_if{|key, value| AVAILABLE_CONTROLS[kind.to_sym].include?(key.to_sym) }.invert
+  end
 end
