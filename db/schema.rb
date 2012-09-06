64e03a77ce7c6e4de78cc51dabc0ffaa497e4e4f
Dmitry Lihachev <lda@openteam.ru>
2012-09-06 11:01:18 +0700

    новая схема

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120905070954) do
+ActiveRecord::Schema.define(:version => 20120905092903) do
 
   create_table "appendix_items", :force => true do |t|
     t.integer  "appendix_id"
