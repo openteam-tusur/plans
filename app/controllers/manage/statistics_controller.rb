705a7d0fec9b839ba18c389e77468d07157d7132
Dmitry Lihachev <lda@openteam.ru>
2012-12-06 13:55:56 +0700

    починка статистики

--- app/controllers/manage/statistics_controller.rb
+++ app/controllers/manage/statistics_controller.rb
@@ -1,5 +1,6 @@
-class Manage::StatisticsController < Manage::ApplicationController
-  esp_load_and_authorize_resource
+class Manage::StatisticsController < ApplicationController
+  before_filter :authenticate_user!
+  layout 'manage'
 
   def index
     @year = Year.find_by_number(params[:year_id])
