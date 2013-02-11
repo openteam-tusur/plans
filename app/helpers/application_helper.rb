27d617d6b060a56c1872214ac8dd9caea390c746
Dmitry Lihachev <lda@openteam.ru>
2013-02-11 16:37:48 +0700

    отображать ссылку на РУП при просмотре модули РУП

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def work_plan_api_url(work_plan)
+    "#{root_url}api/v1/work_plans/#{work_plan.id}"
+  end
+
   def indicate_validity(item)
     "#{item} " << (@work_programm.as_json[:validations][item] ? 'success' : 'warning')
   end
