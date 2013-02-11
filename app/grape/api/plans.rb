fa5f3309e18f4009892f072f8d75dcf3e1b85c5c
Dmitry Lihachev <lda@openteam.ru>
2013-02-11 14:47:13 +0700

    неизменяемый url для РУП

--- app/grape/api/plans.rb
+++ app/grape/api/plans.rb
@@ -2,29 +2,31 @@
 
 class API::Plans < Grape::API
   version 'v1', :using => :path
-  rescue_from :all
+
+  rescue_from ActiveRecord::RecordNotFound
+
   format :json
 
   get :ping do
     :ok
   end
 
+  helpers do
+    def subspeciality
+      @subspeciality ||= Subspeciality.actual.find(params[:subspeciality_id])
+    end
+
+    def semester
+      subspeciality.semesters.actual.find_by_number!(params[:semester])
+    end
+  end
+
   namespace :disciplines do
-    helpers do
-      def subspeciality
-        @subspeciality ||= Subspeciality.actual.find(params[:supspeciality_id])
-      end
-
-      def semester
-        subspeciality.semesters.actual.find_by_number(params[:semester])
-      end
-    end
-
     params do
-      requires :supspeciality_id, :type => Integer, :desc => "id of subspeciality"
+      requires :subspeciality_id, :type => Integer, :desc => "id of subspeciality"
     end
 
-    namespace ":supspeciality_id" do
+    namespace ":subspeciality_id" do
       desc 'Retrieve subspeciality disciplines in semester'
       params do
         requires :semester, :type => Integer, :desc => "number of semester"
@@ -32,6 +34,16 @@ class API::Plans < Grape::API
       get ":semester" do
         present semester.disciplines.actual, :with => API::Entities::Discipline, :semester => semester
       end
+    end
+  end
+
+  desc 'Redirect to work_plan'
+  namespace :work_plans do
+    params do
+      requires :subspeciality_id, :type => Integer, :desc => "id of subspeciality"
+    end
+    get ":subspeciality_id" do
+      redirect subspeciality.work_plan.try :file_url
     end
   end
 end
