869f09354301ddde9d265c5c668603ad5a4d8958
Dmitry Lihachev <lda@openteam.ru>
2013-02-22 09:08:58 +0700

    disable debugging of assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Plans::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.generators do |generators|
     generators.assets               false
