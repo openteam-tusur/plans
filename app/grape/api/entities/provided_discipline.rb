f8d27cbea9d3dea0b932c7c1206ffebe2762ab31
Manin Iliy <bkmzvf@gmail.com>
2013-05-16 09:52:22 +0700

    fix

--- app/grape/api/entities/provided_discipline.rb
+++ app/grape/api/entities/provided_discipline.rb
@@ -4,11 +4,11 @@ class API::Entities::ProvidedDiscipline < Grape::Entity
   expose :abbr
   expose(:goses) { |model, options|
     hash = {}
-    model.disciplines.actual.order('title ASC').each do |discipline|
+    model.disciplines.actual.order('ascii(title) ASC').each do |discipline|
       hash[discipline.speciality.gos_generation] ||= {}
       hash[discipline.speciality.gos_generation]["#{discipline.speciality.title} (#{discipline.speciality.degree_text})"] ||= {}
-      hash[discipline.speciality.gos_generation]["#{discipline.speciality.title} (#{discipline.speciality.degree_text})"]["Набор #{discipline.speciality.year.number} года и последующих лет, #{discipline.subspeciality.education_form_text} форма обучения"] ||= []
-      hash[discipline.speciality.gos_generation]["#{discipline.speciality.title} (#{discipline.speciality.degree_text})"]["Набор #{discipline.speciality.year.number} года и последующих лет, #{discipline.subspeciality.education_form_text} форма обучения"] << discipline.title.gsub(/\s*-\s*/, ' - ')
+      hash[discipline.speciality.gos_generation]["#{discipline.speciality.title} (#{discipline.speciality.degree_text})"]["Набор #{discipline.speciality.year.number} года и последующих лет, #{discipline.subspeciality.education_form_text} форма обучения #{discipline.subspeciality.reduced_text}".strip] ||= []
+      hash[discipline.speciality.gos_generation]["#{discipline.speciality.title} (#{discipline.speciality.degree_text})"]["Набор #{discipline.speciality.year.number} года и последующих лет, #{discipline.subspeciality.education_form_text} форма обучения #{discipline.subspeciality.reduced_text}".strip] << discipline.title.gsub(/\s*-\s*/, ' - ')
     end
     hash
   }
