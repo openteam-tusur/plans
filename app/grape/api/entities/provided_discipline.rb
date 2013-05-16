d78d150fa972bd15d0fc5512b5eee97ddc3eda20
Manin Iliy <bkmzvf@gmail.com>
2013-05-16 11:11:32 +0700

    Added speciality code

--- app/grape/api/entities/provided_discipline.rb
+++ app/grape/api/entities/provided_discipline.rb
@@ -7,9 +7,9 @@ class API::Entities::ProvidedDiscipline < Grape::Entity
     model.disciplines.actual.order('title ASC').each do |discipline|
       title = discipline.title.gsub(/\s*-\s*/, ' - ')
       hash[discipline.speciality.gos_generation] ||= {}
-      hash[discipline.speciality.gos_generation]["#{discipline.speciality.title} (#{discipline.speciality.degree_text})"] ||= {}
-      hash[discipline.speciality.gos_generation]["#{discipline.speciality.title} (#{discipline.speciality.degree_text})"]["Набор #{discipline.speciality.year.number} года и последующих лет, #{discipline.subspeciality.education_form_text} форма обучения #{discipline.subspeciality.reduced_text}".strip] ||= {}
-      hash[discipline.speciality.gos_generation]["#{discipline.speciality.title} (#{discipline.speciality.degree_text})"]["Набор #{discipline.speciality.year.number} года и последующих лет, #{discipline.subspeciality.education_form_text} форма обучения #{discipline.subspeciality.reduced_text}".strip][title] = discipline.work_programms.last.try(:file_url)
+      hash[discipline.speciality.gos_generation]["#{discipline.speciality.code}.#{discipline.speciality.degree} #{discipline.speciality.title} (#{discipline.speciality.degree_text})"] ||= {}
+      hash[discipline.speciality.gos_generation]["#{discipline.speciality.code}.#{discipline.speciality.degree} #{discipline.speciality.title} (#{discipline.speciality.degree_text})"]["Набор #{discipline.speciality.year.number} года и последующих лет, #{discipline.subspeciality.education_form_text} форма обучения #{discipline.subspeciality.reduced_text}".strip] ||= {}
+      hash[discipline.speciality.gos_generation]["#{discipline.speciality.code}.#{discipline.speciality.degree} #{discipline.speciality.title} (#{discipline.speciality.degree_text})"]["Набор #{discipline.speciality.year.number} года и последующих лет, #{discipline.subspeciality.education_form_text} форма обучения #{discipline.subspeciality.reduced_text}".strip][title] = discipline.work_programms.last.try(:file_url)
     end
     hash
   }
